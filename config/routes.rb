01661c37a4d12efef256ea9e088a8411d20592ce
Kai Xiang <kaixiang@refvy.com>
2016-01-15 14:40:25 +0000

    add friendship and friend search functionality

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   get 'my_friends', to: "users#my_friends"
   resources :user, only: [:show]
   resources :friendship
+  get 'search_friends', to: "users#search"
+  post 'add_friend', to: "users#add_friend"
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
